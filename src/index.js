0b3c13c9f4482d2a6c30925a95db93f17ee6d147
Mehboob Alam <mehboobalam.dev@gmail.com>
2023-09-04 07:31:35 +0500

    creating component and display on the screen

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import "./index.css"
+function Hi(){
+  return <div>Hellow World</div>
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const Card = ({name, url, body} ) => (
+  <div className='mediaCard'>
+    <h6>{name}</h6>
+    <p>{body}</p>
+    <img style={{padding: "200px"}} src={url}  alt=''/>
+
+  </div>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const MediaCard = ()=>(
+  <>
+    <Card name="Mehboob Alam" body="A Web Developer" url="https://t3.ftcdn.net/jpg/03/46/83/96/360_F_346839683_6nAPzbhpSkIpb8pmAwufkC7c5eD7wYws.jpg"/>
+    <Card name="Shahzad " body="A Finance" url="https://images.pexels.com/photos/771742/pexels-photo-771742.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500"/>
+    <Card name="M Shan" body="An Economist" url="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"/>
+  </>
+)
+
+ReactDOM.render(<MediaCard/>, document.querySelector('#root'));
